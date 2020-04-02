4adcca19e2ae7065ee20401f1b184e4e8edf04e4
Rin81401101 <senkou.8140989@gmail.com>
2020-04-02 14:35:29 +0900

    α制作 ステージ回転・キャラ操作

---
+++ Tokyo Game Award/Assets/C#Script/InversAnimation.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InversAnimation : MonoBehaviour
+{
+    //定義
+    private Animator animator;
+    private bool isInvers;
+    private bool isReInvers;
+    private bool isIdle;
+    private float freezetime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //初期化
+        animator = GetComponent<Animator>();
+        isInvers = true;
+        isReInvers = true;
+        freezetime = 1.0f;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //回転
+        if (Input.GetKeyDown(KeyCode.Space) && isInvers == true) {
+            PlayerController.rig.constraints = RigidbodyConstraints.FreezeAll;  //全固定
+            animator.SetTrigger("isInvers");                                    //アニメーション
+            Invoke("Freeze", freezetime);                                       //遅延処理
+            isInvers = false;
+            
+
+        }
+        //再回転
+        else if (Input.GetKeyDown(KeyCode.Space) && isReInvers == true) {
+            PlayerController.rig.constraints = RigidbodyConstraints.FreezeAll;
+            animator.SetTrigger("isReInvers");
+            Invoke("Freeze", freezetime);
+            isReInvers = false;
+            
+
+        }
+        //通常
+        else if (isInvers == false && isReInvers == false){
+
+            
+            PlayerController.rig.constraints = RigidbodyConstraints.FreezeAll;
+            animator.SetTrigger("isIdle");
+            Invoke("Freeze", freezetime);
+            isInvers = true;
+            isReInvers = true;
+            
+
+        }
+        
+
+        
+
+    }
+
+    //フリーズ処理
+    private void Freeze()
+    {
+        PlayerController.rig.constraints = RigidbodyConstraints.None;           //固定解除
+        PlayerController.rig.constraints = RigidbodyConstraints.FreezeRotation;
+        //PlayerController.rig.constraints = RigidbodyConstraints.FreezeRotation; //回転固定
+
+    }
+}
---
+++ Tokyo Game Award/Assets/C#Script/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static Rigidbody rig;
+    float playerspeed;
+    float jampsSpeed;
+    private bool isJumping;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerspeed = 7.0f;
+        isJumping = false;
+        jampsSpeed = 6.0f;
+        rig = GetComponent<Rigidbody>();
+        //rig.useGravity = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (isJumping == true)
+        {
+
+
+            if (Input.GetKey(KeyCode.X))
+            {
+                rig.velocity += new Vector3(0, jampsSpeed, 0);
+                isJumping = false;
+                
+
+            }
+            //上
+            else if (Input.GetKey(KeyCode.W))
+            {
+
+                rig.velocity = new Vector3(0, 0, playerspeed);
+
+            }
+            //左
+            else if (Input.GetKey(KeyCode.A))
+            {
+
+                rig.velocity = new Vector3(-playerspeed, 0, 0);
+
+            }
+            //下
+            else if (Input.GetKey(KeyCode.S))
+            {
+
+                rig.velocity = new Vector3(0, 0, -playerspeed);
+
+            }
+            //右
+            else if (Input.GetKey(KeyCode.D))
+            {
+
+                rig.velocity = new Vector3(playerspeed, 0, 0);
+            }
+
+            else
+            {
+                rig.velocity += new Vector3(0, 0, 0);
+            }
+        }
+        else
+        {
+            rig.velocity += new Vector3(0, 0, 0);
+
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Stage"))
+        {
+            isJumping = true;
+        }
+
+    }
+
+    
+}
