02d961ec204aeeb22d579688b611d206d5f96edb
harentiman <harentiman@gmail.com>
2020-05-03 17:58:36 +0900

    マスターデータ更新、旧型マスター追加。 マスターデータ更新、リトライの仮設置。

---
+++ SIN_GameAward/Assets/C#/Retry.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Retry : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Retry_Zone")
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+}
